1cd29080a66fe130682d2379a7d2396960765265
nstoqnov <peac36@abv.bg>
2019-03-07 21:59:46 +0200

    Add provider class

---
+++ src/Provider.php
@@ -0,0 +1,17 @@
+<?php
+namespace Peac36\Explorer;
+
+use Illuminate\Support\ServiceProvider;
+
+class Provider extends ServiceProvider
+{
+    public function boot()
+    {
+
+    }
+
+    public function register()
+    {
+
+    }
+}
